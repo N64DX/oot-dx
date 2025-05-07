0b999d3ba07d8be8623d6eb27bfac79f50cf683f
GhostlyDark <31742919+GhostlyDark@users.noreply.github.com>
2025-05-07 22:21:48 +0200

    Remove PAUSE_BG_AA from config

--- src/overlays/misc/ovl_kaleido_scope/z_kaleido_scope.c
+++ src/overlays/misc/ovl_kaleido_scope/z_kaleido_scope.c
@@ -963,11 +963,7 @@ void KaleidoScope_SetupPlayerPreRender(PlayState* play) {
 }
 
 void KaleidoScope_ProcessPlayerPreRender(void) {
-#if PAUSE_BG_AA
     Sleep_Msec(50);
-    PreRender_ApplyFilters(&sPlayerPreRender);
-    PreRender_Destroy(&sPlayerPreRender);
-#endif
 }
 
 Gfx* KaleidoScope_QuadTextureIA4(Gfx* gfx, void* texture, s16 width, s16 height, u16 point) {
