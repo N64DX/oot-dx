fed77815e385fa391681685cf05078a5ee317452
GhostlyDark <31742919+GhostlyDark@users.noreply.github.com>
2025-06-02 19:17:47 +0200

    Fix more build errors

---
+++ assets/scenes/misc/shrine_n/shrine_n_scene_02000090_EnvLightSettingsList.inc.c
@@ -0,0 +1,11 @@
+    {
+        {  25,  25,  25 }, // ambientColor
+        {  -77,   47,   88 }, // light1Dir
+        { 160, 160, 130 }, // light1Color
+        {  -25,  103,  -69 }, // light2Dir
+        { 190, 200, 210 }, // light2Color
+        {  18,  18,  28 }, // fogColor
+        ((4 / 4) << 10) | 970, // blendRateAndFogNear
+        3000, // zFar
+    }, // 0
+
---
+++ assets/scenes/misc/shrine_r/shrine_r_scene_02000088_EnvLightSettingsList.inc.c
@@ -0,0 +1,11 @@
+    {
+        {  30,  30,  30 }, // ambientColor
+        {  -57,   96,  -58 }, // light1Dir
+        { 170, 140, 160 }, // light1Color
+        {   80,   60,   76 }, // light2Dir
+        { 120,  90, 100 }, // light2Color
+        {  50,  50,  55 }, // fogColor
+        ((4 / 4) << 10) | 970, // blendRateAndFogNear
+        3000, // zFar
+    }, // 0
+
