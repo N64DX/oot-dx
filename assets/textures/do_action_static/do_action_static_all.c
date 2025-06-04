3fb2fd9f7d65261a698ad146ac4c91dccb118983
GhostlyDark <31742919+GhostlyDark@users.noreply.github.com>
2025-06-04 20:12:18 +0200

    Fix do_action_static

--- assets/textures/do_action_static/do_action_static_all.c
+++ assets/textures/do_action_static/do_action_static_all.c
@@ -1,119 +1,119 @@
 #include "do_action_static_all.h"
 
 u64 gAttackDoActionENGTex[TEX_LEN(u64, gAttackDoActionENGTex_WIDTH, gAttackDoActionENGTex_HEIGHT, 4)] = {
-#include "build/pal-1.0/assets/textures/do_action_static/gAttackDoActionENGTex.ia4.inc.c"
+#include "assets/textures/do_action_static/gAttackDoActionENGTex.ia4.inc.c"
 };
 
 u64 gCheckDoActionENGTex[TEX_LEN(u64, gCheckDoActionENGTex_WIDTH, gCheckDoActionENGTex_HEIGHT, 4)] = {
-#include "build/pal-1.0/assets/textures/do_action_static/gCheckDoActionENGTex.ia4.inc.c"
+#include "assets/textures/do_action_static/gCheckDoActionENGTex.ia4.inc.c"
 };
 
 u64 gEnterDoActionENGTex[TEX_LEN(u64, gEnterDoActionENGTex_WIDTH, gEnterDoActionENGTex_HEIGHT, 4)] = {
-#include "build/pal-1.0/assets/textures/do_action_static/gEnterDoActionENGTex.ia4.inc.c"
+#include "assets/textures/do_action_static/gEnterDoActionENGTex.ia4.inc.c"
 };
 
 u64 gReturnDoActionENGTex[TEX_LEN(u64, gReturnDoActionENGTex_WIDTH, gReturnDoActionENGTex_HEIGHT, 4)] = {
-#include "build/pal-1.0/assets/textures/do_action_static/gReturnDoActionENGTex.ia4.inc.c"
+#include "assets/textures/do_action_static/gReturnDoActionENGTex.ia4.inc.c"
 };
 
 u64 gOpenDoActionENGTex[TEX_LEN(u64, gOpenDoActionENGTex_WIDTH, gOpenDoActionENGTex_HEIGHT, 4)] = {
-#include "build/pal-1.0/assets/textures/do_action_static/gOpenDoActionENGTex.ia4.inc.c"
+#include "assets/textures/do_action_static/gOpenDoActionENGTex.ia4.inc.c"
 };
 
 u64 gJumpDoActionENGTex[TEX_LEN(u64, gJumpDoActionENGTex_WIDTH, gJumpDoActionENGTex_HEIGHT, 4)] = {
-#include "build/pal-1.0/assets/textures/do_action_static/gJumpDoActionENGTex.ia4.inc.c"
+#include "assets/textures/do_action_static/gJumpDoActionENGTex.ia4.inc.c"
 };
 
 u64 gDecideDoActionENGTex[TEX_LEN(u64, gDecideDoActionENGTex_WIDTH, gDecideDoActionENGTex_HEIGHT, 4)] = {
-#include "build/pal-1.0/assets/textures/do_action_static/gDecideDoActionENGTex.ia4.inc.c"
+#include "assets/textures/do_action_static/gDecideDoActionENGTex.ia4.inc.c"
 };
 
 u64 gDiveDoActionENGTex[TEX_LEN(u64, gDiveDoActionENGTex_WIDTH, gDiveDoActionENGTex_HEIGHT, 4)] = {
-#include "build/pal-1.0/assets/textures/do_action_static/gDiveDoActionENGTex.ia4.inc.c"
+#include "assets/textures/do_action_static/gDiveDoActionENGTex.ia4.inc.c"
 };
 
 u64 gFasterDoActionENGTex[TEX_LEN(u64, gFasterDoActionENGTex_WIDTH, gFasterDoActionENGTex_HEIGHT, 4)] = {
-#include "build/pal-1.0/assets/textures/do_action_static/gFasterDoActionENGTex.ia4.inc.c"
+#include "assets/textures/do_action_static/gFasterDoActionENGTex.ia4.inc.c"
 };
 
 u64 gThrowDoActionENGTex[TEX_LEN(u64, gThrowDoActionENGTex_WIDTH, gThrowDoActionENGTex_HEIGHT, 4)] = {
-#include "build/pal-1.0/assets/textures/do_action_static/gThrowDoActionENGTex.ia4.inc.c"
+#include "assets/textures/do_action_static/gThrowDoActionENGTex.ia4.inc.c"
 };
 
 u64 gUnusedNaviDoActionENGTex[TEX_LEN(u64, gUnusedNaviDoActionENGTex_WIDTH, gUnusedNaviDoActionENGTex_HEIGHT, 4)] = {
-#include "build/pal-1.0/assets/textures/do_action_static/gUnusedNaviDoActionENGTex.ia4.inc.c"
+#include "assets/textures/do_action_static/gUnusedNaviDoActionENGTex.ia4.inc.c"
 };
 
 u64 gClimbDoActionENGTex[TEX_LEN(u64, gClimbDoActionENGTex_WIDTH, gClimbDoActionENGTex_HEIGHT, 4)] = {
-#include "build/pal-1.0/assets/textures/do_action_static/gClimbDoActionENGTex.ia4.inc.c"
+#include "assets/textures/do_action_static/gClimbDoActionENGTex.ia4.inc.c"
 };
 
 u64 gDropDoActionENGTex[TEX_LEN(u64, gDropDoActionENGTex_WIDTH, gDropDoActionENGTex_HEIGHT, 4)] = {
-#include "build/pal-1.0/assets/textures/do_action_static/gDropDoActionENGTex.ia4.inc.c"
+#include "assets/textures/do_action_static/gDropDoActionENGTex.ia4.inc.c"
 };
 
 u64 gDownDoActionENGTex[TEX_LEN(u64, gDownDoActionENGTex_WIDTH, gDownDoActionENGTex_HEIGHT, 4)] = {
-#include "build/pal-1.0/assets/textures/do_action_static/gDownDoActionENGTex.ia4.inc.c"
+#include "assets/textures/do_action_static/gDownDoActionENGTex.ia4.inc.c"
 };
 
 u64 gSaveDoActionENGTex[TEX_LEN(u64, gSaveDoActionENGTex_WIDTH, gSaveDoActionENGTex_HEIGHT, 4)] = {
-#include "build/pal-1.0/assets/textures/do_action_static/gSaveDoActionENGTex.ia4.inc.c"
+#include "assets/textures/do_action_static/gSaveDoActionENGTex.ia4.inc.c"
 };
 
 u64 gSpeakDoActionENGTex[TEX_LEN(u64, gSpeakDoActionENGTex_WIDTH, gSpeakDoActionENGTex_HEIGHT, 4)] = {
-#include "build/pal-1.0/assets/textures/do_action_static/gSpeakDoActionENGTex.ia4.inc.c"
+#include "assets/textures/do_action_static/gSpeakDoActionENGTex.ia4.inc.c"
 };
 
 u64 gNextDoActionENGTex[TEX_LEN(u64, gNextDoActionENGTex_WIDTH, gNextDoActionENGTex_HEIGHT, 4)] = {
-#include "build/pal-1.0/assets/textures/do_action_static/gNextDoActionENGTex.ia4.inc.c"
+#include "assets/textures/do_action_static/gNextDoActionENGTex.ia4.inc.c"
 };
 
 u64 gGrabDoActionENGTex[TEX_LEN(u64, gGrabDoActionENGTex_WIDTH, gGrabDoActionENGTex_HEIGHT, 4)] = {
-#include "build/pal-1.0/assets/textures/do_action_static/gGrabDoActionENGTex.ia4.inc.c"
+#include "assets/textures/do_action_static/gGrabDoActionENGTex.ia4.inc.c"
 };
 
 u64 gStopDoActionENGTex[TEX_LEN(u64, gStopDoActionENGTex_WIDTH, gStopDoActionENGTex_HEIGHT, 4)] = {
-#include "build/pal-1.0/assets/textures/do_action_static/gStopDoActionENGTex.ia4.inc.c"
+#include "assets/textures/do_action_static/gStopDoActionENGTex.ia4.inc.c"
 };
 
 u64 gPutAwayDoActionENGTex[TEX_LEN(u64, gPutAwayDoActionENGTex_WIDTH, gPutAwayDoActionENGTex_HEIGHT, 4)] = {
-#include "build/pal-1.0/assets/textures/do_action_static/gPutAwayDoActionENGTex.ia4.inc.c"
+#include "assets/textures/do_action_static/gPutAwayDoActionENGTex.ia4.inc.c"
 };
 
 u64 gReelDoActionENGTex[TEX_LEN(u64, gReelDoActionENGTex_WIDTH, gReelDoActionENGTex_HEIGHT, 4)] = {
-#include "build/pal-1.0/assets/textures/do_action_static/gReelDoActionENGTex.ia4.inc.c"
+#include "assets/textures/do_action_static/gReelDoActionENGTex.ia4.inc.c"
 };
 
 u64 gNum1DoActionENGTex[TEX_LEN(u64, gNum1DoActionENGTex_WIDTH, gNum1DoActionENGTex_HEIGHT, 4)] = {
-#include "build/pal-1.0/assets/textures/do_action_static/gNum1DoActionENGTex.ia4.inc.c"
+#include "assets/textures/do_action_static/gNum1DoActionENGTex.ia4.inc.c"
 };
 
 u64 gNum2DoActionENGTex[TEX_LEN(u64, gNum2DoActionENGTex_WIDTH, gNum2DoActionENGTex_HEIGHT, 4)] = {
-#include "build/pal-1.0/assets/textures/do_action_static/gNum2DoActionENGTex.ia4.inc.c"
+#include "assets/textures/do_action_static/gNum2DoActionENGTex.ia4.inc.c"
 };
 
 u64 gNum3DoActionENGTex[TEX_LEN(u64, gNum3DoActionENGTex_WIDTH, gNum3DoActionENGTex_HEIGHT, 4)] = {
-#include "build/pal-1.0/assets/textures/do_action_static/gNum3DoActionENGTex.ia4.inc.c"
+#include "assets/textures/do_action_static/gNum3DoActionENGTex.ia4.inc.c"
 };
 
 u64 gNum4DoActionENGTex[TEX_LEN(u64, gNum4DoActionENGTex_WIDTH, gNum4DoActionENGTex_HEIGHT, 4)] = {
-#include "build/pal-1.0/assets/textures/do_action_static/gNum4DoActionENGTex.ia4.inc.c"
+#include "assets/textures/do_action_static/gNum4DoActionENGTex.ia4.inc.c"
 };
 
 u64 gNum5DoActionENGTex[TEX_LEN(u64, gNum5DoActionENGTex_WIDTH, gNum5DoActionENGTex_HEIGHT, 4)] = {
-#include "build/pal-1.0/assets/textures/do_action_static/gNum5DoActionENGTex.ia4.inc.c"
+#include "assets/textures/do_action_static/gNum5DoActionENGTex.ia4.inc.c"
 };
 
 u64 gNum6DoActionENGTex[TEX_LEN(u64, gNum6DoActionENGTex_WIDTH, gNum6DoActionENGTex_HEIGHT, 4)] = {
-#include "build/pal-1.0/assets/textures/do_action_static/gNum6DoActionENGTex.ia4.inc.c"
+#include "assets/textures/do_action_static/gNum6DoActionENGTex.ia4.inc.c"
 };
 
 u64 gNum7DoActionENGTex[TEX_LEN(u64, gNum7DoActionENGTex_WIDTH, gNum7DoActionENGTex_HEIGHT, 4)] = {
-#include "build/pal-1.0/assets/textures/do_action_static/gNum7DoActionENGTex.ia4.inc.c"
+#include "assets/textures/do_action_static/gNum7DoActionENGTex.ia4.inc.c"
 };
 
 u64 gNum8DoActionENGTex[TEX_LEN(u64, gNum8DoActionENGTex_WIDTH, gNum8DoActionENGTex_HEIGHT, 4)] = {
-#include "build/pal-1.0/assets/textures/do_action_static/gNum8DoActionENGTex.ia4.inc.c"
+#include "assets/textures/do_action_static/gNum8DoActionENGTex.ia4.inc.c"
 };
 
 u64 gAttackDoActionGERTex[TEX_LEN(u64, gAttackDoActionGERTex_WIDTH, gAttackDoActionGERTex_HEIGHT, 4)] = {
@@ -349,117 +349,117 @@ u64 gNum8DoActionFRATex[TEX_LEN(u64, gNum8DoActionFRATex_WIDTH, gNum8DoActionFRA
 };
 
 u64 gAttackDoActionJPNTex[TEX_LEN(u64, gAttackDoActionJPNTex_WIDTH, gAttackDoActionJPNTex_HEIGHT, 4)] = {
-#include "build/pal-1.0/assets/textures/do_action_static/gAttackDoActionJPNTex.ia4.inc.c"
+#include "assets/textures/do_action_static/gAttackDoActionJPNTex.ia4.inc.c"
 };
 
 u64 gCheckDoActionJPNTex[TEX_LEN(u64, gCheckDoActionJPNTex_WIDTH, gCheckDoActionJPNTex_HEIGHT, 4)] = {
-#include "build/pal-1.0/assets/textures/do_action_static/gCheckDoActionJPNTex.ia4.inc.c"
+#include "assets/textures/do_action_static/gCheckDoActionJPNTex.ia4.inc.c"
 };
 
 u64 gEnterDoActionJPNTex[TEX_LEN(u64, gEnterDoActionJPNTex_WIDTH, gEnterDoActionJPNTex_HEIGHT, 4)] = {
-#include "build/pal-1.0/assets/textures/do_action_static/gEnterDoActionJPNTex.ia4.inc.c"
+#include "assets/textures/do_action_static/gEnterDoActionJPNTex.ia4.inc.c"
 };
 
 u64 gReturnDoActionJPNTex[TEX_LEN(u64, gReturnDoActionJPNTex_WIDTH, gReturnDoActionJPNTex_HEIGHT, 4)] = {
-#include "build/pal-1.0/assets/textures/do_action_static/gReturnDoActionJPNTex.ia4.inc.c"
+#include "assets/textures/do_action_static/gReturnDoActionJPNTex.ia4.inc.c"
 };
 
 u64 gOpenDoActionJPNTex[TEX_LEN(u64, gOpenDoActionJPNTex_WIDTH, gOpenDoActionJPNTex_HEIGHT, 4)] = {
-#include "build/pal-1.0/assets/textures/do_action_static/gOpenDoActionJPNTex.ia4.inc.c"
+#include "assets/textures/do_action_static/gOpenDoActionJPNTex.ia4.inc.c"
 };
 
 u64 gJumpDoActionJPNTex[TEX_LEN(u64, gJumpDoActionJPNTex_WIDTH, gJumpDoActionJPNTex_HEIGHT, 4)] = {
-#include "build/pal-1.0/assets/textures/do_action_static/gJumpDoActionJPNTex.ia4.inc.c"
+#include "assets/textures/do_action_static/gJumpDoActionJPNTex.ia4.inc.c"
 };
 
 u64 gDecideDoActionJPNTex[TEX_LEN(u64, gDecideDoActionJPNTex_WIDTH, gDecideDoActionJPNTex_HEIGHT, 4)] = {
-#include "build/pal-1.0/assets/textures/do_action_static/gDecideDoActionJPNTex.ia4.inc.c"
+#include "assets/textures/do_action_static/gDecideDoActionJPNTex.ia4.inc.c"
 };
 
 u64 gDiveDoActionJPNTex[TEX_LEN(u64, gDiveDoActionJPNTex_WIDTH, gDiveDoActionJPNTex_HEIGHT, 4)] = {
-#include "build/pal-1.0/assets/textures/do_action_static/gDiveDoActionJPNTex.ia4.inc.c"
+#include "assets/textures/do_action_static/gDiveDoActionJPNTex.ia4.inc.c"
 };
 
 u64 gFasterDoActionJPNTex[TEX_LEN(u64, gFasterDoActionJPNTex_WIDTH, gFasterDoActionJPNTex_HEIGHT, 4)] = {
-#include "build/pal-1.0/assets/textures/do_action_static/gFasterDoActionJPNTex.ia4.inc.c"
+#include "assets/textures/do_action_static/gFasterDoActionJPNTex.ia4.inc.c"
 };
 
 u64 gThrowDoActionJPNTex[TEX_LEN(u64, gThrowDoActionJPNTex_WIDTH, gThrowDoActionJPNTex_HEIGHT, 4)] = {
-#include "build/pal-1.0/assets/textures/do_action_static/gThrowDoActionJPNTex.ia4.inc.c"
+#include "assets/textures/do_action_static/gThrowDoActionJPNTex.ia4.inc.c"
 };
 
 u64 gUnusedNaviDoActionJPNTex[TEX_LEN(u64, gUnusedNaviDoActionJPNTex_WIDTH, gUnusedNaviDoActionJPNTex_HEIGHT, 4)] = {
-#include "build/pal-1.0/assets/textures/do_action_static/gUnusedNaviDoActionJPNTex.ia4.inc.c"
+#include "assets/textures/do_action_static/gUnusedNaviDoActionJPNTex.ia4.inc.c"
 };
 
 u64 gClimbDoActionJPNTex[TEX_LEN(u64, gClimbDoActionJPNTex_WIDTH, gClimbDoActionJPNTex_HEIGHT, 4)] = {
-#include "build/pal-1.0/assets/textures/do_action_static/gClimbDoActionJPNTex.ia4.inc.c"
+#include "assets/textures/do_action_static/gClimbDoActionJPNTex.ia4.inc.c"
 };
 
 u64 gDropDoActionJPNTex[TEX_LEN(u64, gDropDoActionJPNTex_WIDTH, gDropDoActionJPNTex_HEIGHT, 4)] = {
-#include "build/pal-1.0/assets/textures/do_action_static/gDropDoActionJPNTex.ia4.inc.c"
+#include "assets/textures/do_action_static/gDropDoActionJPNTex.ia4.inc.c"
 };
 
 u64 gDownDoActionJPNTex[TEX_LEN(u64, gDownDoActionJPNTex_WIDTH, gDownDoActionJPNTex_HEIGHT, 4)] = {
-#include "build/pal-1.0/assets/textures/do_action_static/gDownDoActionJPNTex.ia4.inc.c"
+#include "assets/textures/do_action_static/gDownDoActionJPNTex.ia4.inc.c"
 };
 
 u64 gSaveDoActionJPNTex[TEX_LEN(u64, gSaveDoActionJPNTex_WIDTH, gSaveDoActionJPNTex_HEIGHT, 4)] = {
-#include "build/pal-1.0/assets/textures/do_action_static/gSaveDoActionJPNTex.ia4.inc.c"
+#include "assets/textures/do_action_static/gSaveDoActionJPNTex.ia4.inc.c"
 };
 
 u64 gSpeakDoActionJPNTex[TEX_LEN(u64, gSpeakDoActionJPNTex_WIDTH, gSpeakDoActionJPNTex_HEIGHT, 4)] = {
-#include "build/pal-1.0/assets/textures/do_action_static/gSpeakDoActionJPNTex.ia4.inc.c"
+#include "assets/textures/do_action_static/gSpeakDoActionJPNTex.ia4.inc.c"
 };
 
 u64 gNextDoActionJPNTex[TEX_LEN(u64, gNextDoActionJPNTex_WIDTH, gNextDoActionJPNTex_HEIGHT, 4)] = {
-#include "build/pal-1.0/assets/textures/do_action_static/gNextDoActionJPNTex.ia4.inc.c"
+#include "assets/textures/do_action_static/gNextDoActionJPNTex.ia4.inc.c"
 };
 
 u64 gGrabDoActionJPNTex[TEX_LEN(u64, gGrabDoActionJPNTex_WIDTH, gGrabDoActionJPNTex_HEIGHT, 4)] = {
-#include "build/pal-1.0/assets/textures/do_action_static/gGrabDoActionJPNTex.ia4.inc.c"
+#include "assets/textures/do_action_static/gGrabDoActionJPNTex.ia4.inc.c"
 };
 
 u64 gStopDoActionJPNTex[TEX_LEN(u64, gStopDoActionJPNTex_WIDTH, gStopDoActionJPNTex_HEIGHT, 4)] = {
-#include "build/pal-1.0/assets/textures/do_action_static/gStopDoActionJPNTex.ia4.inc.c"
+#include "assets/textures/do_action_static/gStopDoActionJPNTex.ia4.inc.c"
 };
 
 u64 gPutAwayDoActionJPNTex[TEX_LEN(u64, gPutAwayDoActionJPNTex_WIDTH, gPutAwayDoActionJPNTex_HEIGHT, 4)] = {
-#include "build/pal-1.0/assets/textures/do_action_static/gPutAwayDoActionJPNTex.ia4.inc.c"
+#include "assets/textures/do_action_static/gPutAwayDoActionJPNTex.ia4.inc.c"
 };
 
 u64 gReelDoActionJPNTex[TEX_LEN(u64, gReelDoActionJPNTex_WIDTH, gReelDoActionJPNTex_HEIGHT, 4)] = {
-#include "build/pal-1.0/assets/textures/do_action_static/gReelDoActionJPNTex.ia4.inc.c"
+#include "assets/textures/do_action_static/gReelDoActionJPNTex.ia4.inc.c"
 };
 
 u64 gNum1DoActionJPNTex[TEX_LEN(u64, gNum1DoActionJPNTex_WIDTH, gNum1DoActionJPNTex_HEIGHT, 4)] = {
-#include "build/pal-1.0/assets/textures/do_action_static/gNum1DoActionJPNTex.ia4.inc.c"
+#include "assets/textures/do_action_static/gNum1DoActionJPNTex.ia4.inc.c"
 };
 
 u64 gNum2DoActionJPNTex[TEX_LEN(u64, gNum2DoActionJPNTex_WIDTH, gNum2DoActionJPNTex_HEIGHT, 4)] = {
-#include "build/pal-1.0/assets/textures/do_action_static/gNum2DoActionJPNTex.ia4.inc.c"
+#include "assets/textures/do_action_static/gNum2DoActionJPNTex.ia4.inc.c"
 };
 
 u64 gNum3DoActionJPNTex[TEX_LEN(u64, gNum3DoActionJPNTex_WIDTH, gNum3DoActionJPNTex_HEIGHT, 4)] = {
-#include "build/pal-1.0/assets/textures/do_action_static/gNum3DoActionJPNTex.ia4.inc.c"
+#include "assets/textures/do_action_static/gNum3DoActionJPNTex.ia4.inc.c"
 };
 
 u64 gNum4DoActionJPNTex[TEX_LEN(u64, gNum4DoActionJPNTex_WIDTH, gNum4DoActionJPNTex_HEIGHT, 4)] = {
-#include "build/pal-1.0/assets/textures/do_action_static/gNum4DoActionJPNTex.ia4.inc.c"
+#include "assets/textures/do_action_static/gNum4DoActionJPNTex.ia4.inc.c"
 };
 
 u64 gNum5DoActionJPNTex[TEX_LEN(u64, gNum5DoActionJPNTex_WIDTH, gNum5DoActionJPNTex_HEIGHT, 4)] = {
-#include "build/pal-1.0/assets/textures/do_action_static/gNum5DoActionJPNTex.ia4.inc.c"
+#include "assets/textures/do_action_static/gNum5DoActionJPNTex.ia4.inc.c"
 };
 
 u64 gNum6DoActionJPNTex[TEX_LEN(u64, gNum6DoActionJPNTex_WIDTH, gNum6DoActionJPNTex_HEIGHT, 4)] = {
-#include "build/pal-1.0/assets/textures/do_action_static/gNum6DoActionJPNTex.ia4.inc.c"
+#include "assets/textures/do_action_static/gNum6DoActionJPNTex.ia4.inc.c"
 };
 
 u64 gNum7DoActionJPNTex[TEX_LEN(u64, gNum7DoActionJPNTex_WIDTH, gNum7DoActionJPNTex_HEIGHT, 4)] = {
-#include "build/pal-1.0/assets/textures/do_action_static/gNum7DoActionJPNTex.ia4.inc.c"
+#include "assets/textures/do_action_static/gNum7DoActionJPNTex.ia4.inc.c"
 };
 
 u64 gNum8DoActionJPNTex[TEX_LEN(u64, gNum8DoActionJPNTex_WIDTH, gNum8DoActionJPNTex_HEIGHT, 4)] = {
-#include "build/pal-1.0/assets/textures/do_action_static/gNum8DoActionJPNTex.ia4.inc.c"
+#include "assets/textures/do_action_static/gNum8DoActionJPNTex.ia4.inc.c"
 };
