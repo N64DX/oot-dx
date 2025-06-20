d324231f468965157eb60f796fafd03eb2ec1067
Dragorn421 <Dragorn421@users.noreply.github.com>
2025-06-20 10:12:45 +0200

    Rename AT `effect` to `hitSpecialEffect` (#2637)
    * Rename AT `effect` to `hitSpecialEffect`
    
    * bss

--- src/overlays/actors/ovl_Boss_Ganon2/z_boss_ganon2.c
+++ src/overlays/actors/ovl_Boss_Ganon2/z_boss_ganon2.c
@@ -35,7 +35,7 @@
 #include "assets/objects/object_ganon2/object_ganon2_title_card.h"
 #endif
 
-#pragma increment_block_number "gc-eu:0 gc-eu-mq:0 gc-jp:0 gc-jp-ce:0 gc-jp-mq:0 gc-us:0 gc-us-mq:0 ique-cn:128" \
+#pragma increment_block_number "gc-eu:128 gc-eu-mq:128 gc-jp:0 gc-jp-ce:0 gc-jp-mq:0 gc-us:0 gc-us-mq:0 ique-cn:128" \
                                "ntsc-1.0:128 ntsc-1.1:128 ntsc-1.2:128 pal-1.0:128 pal-1.1:128"
 
 #define FLAGS                                                                                 \
