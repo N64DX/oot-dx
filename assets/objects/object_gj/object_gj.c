6f040fbf98e67ffb15d5fb0f2234700a59e1ce1e
Admentus <robin.hallink@gmail.com>
2025-10-08 18:15:37 +0200

    Final Ganon Boss object optimizations

---
+++ assets/objects/object_gj/object_gj.c
@@ -0,0 +1,272 @@
+#include "assets/objects/object_gj/object_gj.h"
+
+#include "array_count.h"
+#include "camera.h"
+#include "gfx.h"
+#include "stdbool.h"
+#include "sys_matrix.h"
+#include "ultra64.h"
+
+Vtx gGanonsCastleRubbleAroundArenaDL_06000000_Vtx_fused_[] = {
+#include "assets/objects/object_gj/gGanonsCastleRubbleAroundArenaDL_06000000_Vtx_fused_.inc.c"
+};
+
+Gfx gGanonsCastleRubbleAroundArenaDL[148] = {
+#include "assets/objects/object_gj/gGanonsCastleRubbleAroundArenaDL.inc.c"
+};
+
+BgCamInfo gGanonsCastleRubbleAroundArenaBgCamList[] = {
+#include "assets/objects/object_gj/gGanonsCastleRubbleAroundArenaBgCamList.inc.c"
+};
+
+SurfaceType gGanonsCastleRubbleAroundArenaSurfaceTypes[] = {
+#include "assets/objects/object_gj/gGanonsCastleRubbleAroundArenaSurfaceTypes.inc.c"
+};
+
+CollisionPoly gGanonsCastleRubbleAroundArenaPolyList[] = {
+#include "assets/objects/object_gj/gGanonsCastleRubbleAroundArenaPolyList.inc.c"
+};
+
+Vec3s gGanonsCastleRubbleAroundArenaVtxList[] = {
+#include "assets/objects/object_gj/gGanonsCastleRubbleAroundArenaVtxList.inc.c"
+};
+
+CollisionHeader gGanonsCastleRubbleAroundArenaCol = {
+#include "assets/objects/object_gj/gGanonsCastleRubbleAroundArenaCol.inc.c"
+};
+
+Vtx gGanonsCastleRubble2DL_06001BA0_Vtx_fused_[] = {
+#include "assets/objects/object_gj/gGanonsCastleRubble2DL_06001BA0_Vtx_fused_.inc.c"
+};
+
+Gfx gGanonsCastleRubble2DL[41] = {
+#include "assets/objects/object_gj/gGanonsCastleRubble2DL.inc.c"
+};
+
+BgCamInfo gGanonsCastleRubble2BgCamList[] = {
+#include "assets/objects/object_gj/gGanonsCastleRubble2BgCamList.inc.c"
+};
+
+SurfaceType gGanonsCastleRubble2SurfaceTypes[] = {
+#include "assets/objects/object_gj/gGanonsCastleRubble2SurfaceTypes.inc.c"
+};
+
+CollisionPoly gGanonsCastleRubble2PolyList[] = {
+#include "assets/objects/object_gj/gGanonsCastleRubble2PolyList.inc.c"
+};
+
+Vec3s gGanonsCastleRubble2VtxList[] = {
+#include "assets/objects/object_gj/gGanonsCastleRubble2VtxList.inc.c"
+};
+
+CollisionHeader gGanonsCastleRubble2Col = {
+#include "assets/objects/object_gj/gGanonsCastleRubble2Col.inc.c"
+};
+
+Vtx gGanonsCastleRubble3DL_06001FA0_Vtx_fused_[] = {
+#include "assets/objects/object_gj/gGanonsCastleRubble3DL_06001FA0_Vtx_fused_.inc.c"
+};
+
+Gfx gGanonsCastleRubble3DL[53] = {
+#include "assets/objects/object_gj/gGanonsCastleRubble3DL.inc.c"
+};
+
+BgCamInfo gGanonsCastleRubble3BgCamList[] = {
+#include "assets/objects/object_gj/gGanonsCastleRubble3BgCamList.inc.c"
+};
+
+SurfaceType gGanonsCastleRubble3SurfaceTypes[] = {
+#include "assets/objects/object_gj/gGanonsCastleRubble3SurfaceTypes.inc.c"
+};
+
+CollisionPoly gGanonsCastleRubble3PolyList[] = {
+#include "assets/objects/object_gj/gGanonsCastleRubble3PolyList.inc.c"
+};
+
+Vec3s gGanonsCastleRubble3VtxList[] = {
+#include "assets/objects/object_gj/gGanonsCastleRubble3VtxList.inc.c"
+};
+
+CollisionHeader gGanonsCastleRubble3Col = {
+#include "assets/objects/object_gj/gGanonsCastleRubble3Col.inc.c"
+};
+
+Vtx gGanonsCastleRubble4DL_06002480_Vtx_fused_[] = {
+#include "assets/objects/object_gj/gGanonsCastleRubble4DL_06002480_Vtx_fused_.inc.c"
+};
+
+Gfx gGanonsCastleRubble4DL[41] = {
+#include "assets/objects/object_gj/gGanonsCastleRubble4DL.inc.c"
+};
+
+BgCamInfo gGanonsCastleRubble4BgCamList[] = {
+#include "assets/objects/object_gj/gGanonsCastleRubble4BgCamList.inc.c"
+};
+
+SurfaceType gGanonsCastleRubble4SurfaceTypes[] = {
+#include "assets/objects/object_gj/gGanonsCastleRubble4SurfaceTypes.inc.c"
+};
+
+CollisionPoly gGanonsCastleRubble4PolyList[] = {
+#include "assets/objects/object_gj/gGanonsCastleRubble4PolyList.inc.c"
+};
+
+Vec3s gGanonsCastleRubble4VtxList[] = {
+#include "assets/objects/object_gj/gGanonsCastleRubble4VtxList.inc.c"
+};
+
+CollisionHeader gGanonsCastleRubble4Col = {
+#include "assets/objects/object_gj/gGanonsCastleRubble4Col.inc.c"
+};
+
+Vtx gGanonsCastleRubble5DL_06002880_Vtx_fused_[] = {
+#include "assets/objects/object_gj/gGanonsCastleRubble5DL_06002880_Vtx_fused_.inc.c"
+};
+
+Gfx gGanonsCastleRubble5DL[53] = {
+#include "assets/objects/object_gj/gGanonsCastleRubble5DL.inc.c"
+};
+
+BgCamInfo gGanonsCastleRubble5BgCamList[] = {
+#include "assets/objects/object_gj/gGanonsCastleRubble5BgCamList.inc.c"
+};
+
+SurfaceType gGanonsCastleRubble5SurfaceTypes[] = {
+#include "assets/objects/object_gj/gGanonsCastleRubble5SurfaceTypes.inc.c"
+};
+
+CollisionPoly gGanonsCastleRubble5PolyList[] = {
+#include "assets/objects/object_gj/gGanonsCastleRubble5PolyList.inc.c"
+};
+
+Vec3s gGanonsCastleRubble5VtxList[] = {
+#include "assets/objects/object_gj/gGanonsCastleRubble5VtxList.inc.c"
+};
+
+CollisionHeader gGanonsCastleRubble5Col = {
+#include "assets/objects/object_gj/gGanonsCastleRubble5Col.inc.c"
+};
+
+Vtx gGanonsCastleRubble6DL_06002D60_Vtx[] = {
+#include "assets/objects/object_gj/gGanonsCastleRubble6DL_06002D60_Vtx.inc.c"
+};
+
+Gfx gGanonsCastleRubble6DL[21] = {
+#include "assets/objects/object_gj/gGanonsCastleRubble6DL.inc.c"
+};
+
+BgCamInfo gGanonsCastleRubble6BgCamList[] = {
+#include "assets/objects/object_gj/gGanonsCastleRubble6BgCamList.inc.c"
+};
+
+SurfaceType gGanonsCastleRubble6SurfaceTypes[] = {
+#include "assets/objects/object_gj/gGanonsCastleRubble6SurfaceTypes.inc.c"
+};
+
+CollisionPoly gGanonsCastleRubble6PolyList[] = {
+#include "assets/objects/object_gj/gGanonsCastleRubble6PolyList.inc.c"
+};
+
+Vec3s gGanonsCastleRubble6VtxList[] = {
+#include "assets/objects/object_gj/gGanonsCastleRubble6VtxList.inc.c"
+};
+
+CollisionHeader gGanonsCastleRubble6Col = {
+#include "assets/objects/object_gj/gGanonsCastleRubble6Col.inc.c"
+};
+
+Vtx gGanonsCastleRubble7DL_06003010_Vtx_fused_[] = {
+#include "assets/objects/object_gj/gGanonsCastleRubble7DL_06003010_Vtx_fused_.inc.c"
+};
+
+Gfx gGanonsCastleRubble7DL[41] = {
+#include "assets/objects/object_gj/gGanonsCastleRubble7DL.inc.c"
+};
+
+BgCamInfo gGanonsCastleRubble7BgCamList[] = {
+#include "assets/objects/object_gj/gGanonsCastleRubble7BgCamList.inc.c"
+};
+
+SurfaceType gGanonsCastleRubble7SurfaceTypes[] = {
+#include "assets/objects/object_gj/gGanonsCastleRubble7SurfaceTypes.inc.c"
+};
+
+CollisionPoly gGanonsCastleRubble7PolyList[] = {
+#include "assets/objects/object_gj/gGanonsCastleRubble7PolyList.inc.c"
+};
+
+Vec3s gGanonsCastleRubble7VtxList[] = {
+#include "assets/objects/object_gj/gGanonsCastleRubble7VtxList.inc.c"
+};
+
+CollisionHeader gGanonsCastleRubble7Col = {
+#include "assets/objects/object_gj/gGanonsCastleRubble7Col.inc.c"
+};
+
+Vtx gGanonsCastleRubbleTallDL_06003410_Vtx_fused_[] = {
+#include "assets/objects/object_gj/gGanonsCastleRubbleTallDL_06003410_Vtx_fused_.inc.c"
+};
+
+Gfx gGanonsCastleRubbleTallDL[57] = {
+#include "assets/objects/object_gj/gGanonsCastleRubbleTallDL.inc.c"
+};
+
+BgCamInfo gGanonsCastleRubbleTallBgCamList[] = {
+#include "assets/objects/object_gj/gGanonsCastleRubbleTallBgCamList.inc.c"
+};
+
+SurfaceType gGanonsCastleRubbleTallSurfaceTypes[] = {
+#include "assets/objects/object_gj/gGanonsCastleRubbleTallSurfaceTypes.inc.c"
+};
+
+CollisionPoly gGanonsCastleRubbleTallPolyList[] = {
+#include "assets/objects/object_gj/gGanonsCastleRubbleTallPolyList.inc.c"
+};
+
+Vec3s gGanonsCastleRubbleTallVtxList[] = {
+#include "assets/objects/object_gj/gGanonsCastleRubbleTallVtxList.inc.c"
+};
+
+CollisionHeader gGanonsCastleRubbleTallCol = {
+#include "assets/objects/object_gj/gGanonsCastleRubbleTallCol.inc.c"
+};
+
+u64 gGanonsCastleRubbleAroundArenaDL_00003B20_Tex[TEX_LEN(u64, gGanonsCastleRubbleAroundArenaDL_00003B20_Tex_WIDTH, gGanonsCastleRubbleAroundArenaDL_00003B20_Tex_HEIGHT, 8)] = {
+#include "assets/objects/object_gj/gGanonsCastleRubbleAroundArenaDL_00003B20_Tex.ia8.inc.c"
+};
+
+u64 gGanonsCastleRubbleAroundArenaDL_00003C20_Tex[TEX_LEN(u64, gGanonsCastleRubbleAroundArenaDL_00003C20_Tex_WIDTH, gGanonsCastleRubbleAroundArenaDL_00003C20_Tex_HEIGHT, 4)] = {
+#include "assets/objects/object_gj/gGanonsCastleRubbleAroundArenaDL_00003C20_Tex.i4.inc.c"
+};
+
+u64 gGanonsCastleRubbleAroundArenaDL_00003D20_Tex[TEX_LEN(u64, gGanonsCastleRubbleAroundArenaDL_00003D20_Tex_WIDTH, gGanonsCastleRubbleAroundArenaDL_00003D20_Tex_HEIGHT, 16)] = {
+#include "assets/objects/object_gj/gGanonsCastleRubbleAroundArenaDL_00003D20_Tex.rgba16.inc.c"
+};
+
+u64 gGanonsCastleRubbleAroundArenaDL_00003F20_Tex[TEX_LEN(u64, gGanonsCastleRubbleAroundArenaDL_00003F20_Tex_WIDTH, gGanonsCastleRubbleAroundArenaDL_00003F20_Tex_HEIGHT, 8)] = {
+#include "assets/objects/object_gj/gGanonsCastleRubbleAroundArenaDL_00003F20_Tex.i8.inc.c"
+};
+
+u64 gGanonsCastleRubbleAroundArenaDL_00004F20_Tex[TEX_LEN(u64, gGanonsCastleRubbleAroundArenaDL_00004F20_Tex_WIDTH, gGanonsCastleRubbleAroundArenaDL_00004F20_Tex_HEIGHT, 8)] = {
+#include "assets/objects/object_gj/gGanonsCastleRubbleAroundArenaDL_00004F20_Tex.i8.inc.c"
+};
+
+u64 gGanonsCastleRubbleAroundArenaDL_00005F20_Tex[TEX_LEN(u64, gGanonsCastleRubbleAroundArenaDL_00005F20_Tex_WIDTH, gGanonsCastleRubbleAroundArenaDL_00005F20_Tex_HEIGHT, 8)] = {
+#include "assets/objects/object_gj/gGanonsCastleRubbleAroundArenaDL_00005F20_Tex.i8.inc.c"
+};
+
+u64 gGanonsCastleRubble2DL_00006F20_Tex[TEX_LEN(u64, gGanonsCastleRubble2DL_00006F20_Tex_WIDTH, gGanonsCastleRubble2DL_00006F20_Tex_HEIGHT, 4)] = {
+#include "assets/objects/object_gj/gGanonsCastleRubble2DL_00006F20_Tex.i4.inc.c"
+};
+
+u64 gGanonsCastleRubble2DL_00007320_Tex[TEX_LEN(u64, gGanonsCastleRubble2DL_00007320_Tex_WIDTH, gGanonsCastleRubble2DL_00007320_Tex_HEIGHT, 8)] = {
+#include "assets/objects/object_gj/gGanonsCastleRubble2DL_00007320_Tex.i8.inc.c"
+};
+
+u64 gGanonsCastleRubble3DL_00007720_Tex[TEX_LEN(u64, gGanonsCastleRubble3DL_00007720_Tex_WIDTH, gGanonsCastleRubble3DL_00007720_Tex_HEIGHT, 8)] = {
+#include "assets/objects/object_gj/gGanonsCastleRubble3DL_00007720_Tex.ia8.inc.c"
+};
+
+u64 gGanonsCastleRubble3DL_00007B20_Tex[TEX_LEN(u64, gGanonsCastleRubble3DL_00007B20_Tex_WIDTH, gGanonsCastleRubble3DL_00007B20_Tex_HEIGHT, 8)] = {
+#include "assets/objects/object_gj/gGanonsCastleRubble3DL_00007B20_Tex.i8.inc.c"
+};
