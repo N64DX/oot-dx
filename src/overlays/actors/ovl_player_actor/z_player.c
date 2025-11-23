df84b4343692a21e757cfc2414fc1f8a76abc48e
Admentus <robin.hallink@gmail.com>
2025-11-23 16:31:50 +0100

    No Jump Speed Limit

--- src/overlays/actors/ovl_player_actor/z_player.c
+++ src/overlays/actors/ovl_player_actor/z_player.c
@@ -7648,7 +7648,7 @@ void func_8083DF68(Player* this, f32 arg1, s16 arg2) {
 void func_8083DFE0(Player* this, f32* arg1, s16* arg2) {
     s16 yawDiff = this->yaw - *arg2;
 
-    if (this->meleeWeaponState == 0) {
+    if (this->meleeWeaponState == 0 && !NO_JUMP_SPEED_LIMIT) {
         this->speedXZ = CLAMP(this->speedXZ, -(R_RUN_SPEED_LIMIT / 100.0f), (R_RUN_SPEED_LIMIT / 100.0f));
     }
 
