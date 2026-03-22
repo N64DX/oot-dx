f5bd1ade835f4c90d049ad06ecee8e4f6e58de06
Admentus <robin.hallink@gmail.com>
2026-03-22 09:14:20 +0100

    Fix Water Temple collision - Part 2

---
+++ assets/scenes/dungeons/MIZUsin/cq/actors/room10_actor.inc.c
@@ -0,0 +1,44 @@
+    {
+        ACTOR_BG_MIZU_WATER,
+        {  -1010,    765,     30 }, // pos
+        {  0x0000,  0x0000,  0x0000 }, // rot
+        -0x100 /* 0xFF00 */, // params
+    }, // 0
+    {
+        ACTOR_EN_TITE,
+        {  -1018,    860,    295 }, // pos
+        {  0x0000, -0x8000,  0x0000 }, // rot
+        -0x002 /* 0xFFFE */, // params
+    }, // 1
+    {
+        ACTOR_OBJ_TSUBO,
+        {   -982,    460,    377 }, // pos
+        {  0x0000,  0x0000,  0x0000 }, // rot
+         0x7003, // params
+    }, // 2
+    {
+        ACTOR_OBJ_TSUBO,
+        {  -1019,    460,    379 }, // pos
+        {  0x0000,  0x0000,  0x0000 }, // rot
+         0x7203, // params
+    }, // 3
+    {
+        ACTOR_OBJ_TSUBO,
+        {  -1055,    460,    378 }, // pos
+        {  0x0000,  0x0000,  0x0000 }, // rot
+         0x7403, // params
+    }, // 4
+    {
+        ACTOR_OBJ_SWITCH,
+        {  -1080,    460,   -290 }, // pos
+        {  0x0000,  0x4000,  0x0000 }, // rot
+         0x3B03, // params
+    }, // 5
+    {
+        ACTOR_EN_SIOFUKI,
+        {  -1020,    460,    125 }, // pos
+        {  0x0000,  0x005A,  0x0000 }, // rot
+         0x0EFF, // params
+    }, // 6
+
+    { ACTOR_BG_SPOT00_HANEBASI, { -845, 920, 0 }, { 0, 0, 0x4000 }, 0xFFFE, }, // 7
