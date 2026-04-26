zeldaret/oot

Pause docs: misc docs (#2636)
* doc `KaleidoScope_SetCursorPos`

* doc bug in `KaleidoScope_DrawWorldMap`

* fix anonymous enum

* `D_808321A8` -> `sSavedButtonStatus`

* s16 decimal for `sVtxPageMapDungeonQuads``X/Y/Width/Height`

* Use `LANGUAGE_` enum for indexing language-indexed regs

src/code/z_construct.c
@@ -270,18 +270,18 @@ void Regs_InitDataImpl(void) {
     YREG(47) = 0;
 
 #if OOT_NTSC
-    R_KALEIDO_UNK1(0) = -45;
-    R_KALEIDO_UNK1(1) = -48;
-    R_KALEIDO_UNK2(0) = 16;
-    R_KALEIDO_UNK2(1) = 22;
-    R_KALEIDO_UNK3(0) = -55;
-    R_KALEIDO_UNK3(1) = -53;
-    R_KALEIDO_UNK4(0) = 43;
-    R_KALEIDO_UNK4(1) = 47;
-    R_KALEIDO_UNK5(0) = -33;
-    R_KALEIDO_UNK5(1) = -42;
-    R_KALEIDO_UNK6(0) = -33;
-    R_KALEIDO_UNK6(1) = -37;
+    R_KALEIDO_UNK1(LANGUAGE_JPN) = -45;
+    R_KALEIDO_UNK1(LANGUAGE_ENG) = -48;
+    R_KALEIDO_UNK2(LANGUAGE_JPN) = 16;
+    R_KALEIDO_UNK2(LANGUAGE_ENG) = 22;
+    R_KALEIDO_UNK3(LANGUAGE_JPN) = -55;
+    R_KALEIDO_UNK3(LANGUAGE_ENG) = -53;
+    R_KALEIDO_UNK4(LANGUAGE_JPN) = 43;
+    R_KALEIDO_UNK4(LANGUAGE_ENG) = 47;
+    R_KALEIDO_UNK5(LANGUAGE_JPN) = -33;
+    R_KALEIDO_UNK5(LANGUAGE_ENG) = -42;
+    R_KALEIDO_UNK6(LANGUAGE_JPN) = -33;
+    R_KALEIDO_UNK6(LANGUAGE_ENG) = -37;
 #else
     // Same as above, although these regs are now unused for PAL versions
     YREG(48) = -45;
@@ -369,24 +369,24 @@ void Regs_InitDataImpl(void) {
     ZREG(47) = 1;
 
 #if OOT_NTSC
-    R_START_LABEL_DD(0) = 86;
-    R_START_LABEL_DD(1) = 100;
+    R_START_LABEL_DD(LANGUAGE_JPN) = 86;
+    R_START_LABEL_DD(LANGUAGE_ENG) = 100;
     R_START_LABEL_WIDTH = 0;
     R_START_LABEL_HEIGHT = 0;
-    R_START_LABEL_Y(0) = 21;
-    R_START_LABEL_Y(1) = 20;
-    R_START_LABEL_X(0) = 122;
-    R_START_LABEL_X(1) = 120;
+    R_START_LABEL_Y(LANGUAGE_JPN) = 21;
+    R_START_LABEL_Y(LANGUAGE_ENG) = 20;
+    R_START_LABEL_X(LANGUAGE_JPN) = 122;
+    R_START_LABEL_X(LANGUAGE_ENG) = 120;
 #else
-    R_START_LABEL_DD(0) = 100;
-    R_START_LABEL_DD(1) = 89;
-    R_START_LABEL_DD(2) = 92;
-    R_START_LABEL_Y(0) = 20;
-    R_START_LABEL_Y(1) = 20;
-    R_START_LABEL_Y(2) = 20;
-    R_START_LABEL_X(0) = 120;
-    R_START_LABEL_X(1) = 119;
-    R_START_LABEL_X(2) = 119;
+    R_START_LABEL_DD(LANGUAGE_ENG) = 100;
+    R_START_LABEL_DD(LANGUAGE_GER) = 89;
+    R_START_LABEL_DD(LANGUAGE_FRA) = 92;
+    R_START_LABEL_Y(LANGUAGE_ENG) = 20;
+    R_START_LABEL_Y(LANGUAGE_GER) = 20;
+    R_START_LABEL_Y(LANGUAGE_FRA) = 20;
+    R_START_LABEL_X(LANGUAGE_ENG) = 120;
+    R_START_LABEL_X(LANGUAGE_GER) = 119;
+    R_START_LABEL_X(LANGUAGE_FRA) = 119;
 #endif
 
     R_PAUSE_QUEST_MEDALLION_SHINE_TIME(0) = 1;
@@ -528,14 +528,14 @@ void Regs_InitDataImpl(void) {
     WREG(7) = 0;
 
 #if OOT_NTSC
-    R_B_LABEL_SCALE(0) = 100;
-    R_B_LABEL_SCALE(1) = 109;
-    R_B_LABEL_X(0) = 151;
-    R_B_LABEL_X(1) = 148;
-    R_B_LABEL_Y(0) = 23;
-    R_B_LABEL_Y(1) = 22;
-    R_A_LABEL_Z(0) = -380;
-    R_A_LABEL_Z(1) = -350;
+    R_B_LABEL_SCALE(LANGUAGE_JPN) = 100;
+    R_B_LABEL_SCALE(LANGUAGE_ENG) = 109;
+    R_B_LABEL_X(LANGUAGE_JPN) = 151;
+    R_B_LABEL_X(LANGUAGE_ENG) = 148;
+    R_B_LABEL_Y(LANGUAGE_JPN) = 23;
+    R_B_LABEL_Y(LANGUAGE_ENG) = 22;
+    R_A_LABEL_Z(LANGUAGE_JPN) = -380;
+    R_A_LABEL_Z(LANGUAGE_ENG) = -350;
 #else
     // Same as above, although these regs are now unused in PAL versions
     WREG(8) = 100;
@@ -570,36 +570,36 @@ void Regs_InitDataImpl(void) {
     WREG(36) = 0;
 
 #if OOT_PAL
-    R_B_LABEL_SCALE(0) = 100;
-    R_B_LABEL_SCALE(1) = 99;
-    R_B_LABEL_SCALE(2) = 109;
-    R_B_LABEL_X(0) = B_BUTTON_X - 9;
-    R_B_LABEL_X(1) = B_BUTTON_X - 11;
-    R_B_LABEL_X(2) = B_BUTTON_X - 12;
-    R_B_LABEL_Y(0) = B_BUTTON_Y + 6;
-    R_B_LABEL_Y(1) = B_BUTTON_Y + 5;
-    R_B_LABEL_Y(2) = B_BUTTON_Y + 5;
-    R_A_LABEL_Z(0) = -380;
-    R_A_LABEL_Z(1) = -360;
-    R_A_LABEL_Z(2) = -350;
-    R_KALEIDO_UNK1(0) = -48;
-    R_KALEIDO_UNK1(1) = 16;
-    R_KALEIDO_UNK1(2) = -62;
-    R_KALEIDO_UNK2(0) = 22;
-    R_KALEIDO_UNK2(1) = -84;
-    R_KALEIDO_UNK2(2) = 20;
-    R_KALEIDO_UNK3(0) = -53;
-    R_KALEIDO_UNK3(1) = 40;
-    R_KALEIDO_UNK3(2) = -64;
-    R_KALEIDO_UNK4(0) = 47;
-    R_KALEIDO_UNK4(1) = -84;
-    R_KALEIDO_UNK4(2) = 44;
-    R_KALEIDO_UNK5(0) = -42;
-    R_KALEIDO_UNK5(1) = 32;
-    R_KALEIDO_UNK5(2) = -45;
-    R_KALEIDO_UNK6(0) = -37;
-    R_KALEIDO_UNK6(1) = 30;
-    R_KALEIDO_UNK6(2) = -50;
+    R_B_LABEL_SCALE(LANGUAGE_ENG) = 100;
+    R_B_LABEL_SCALE(LANGUAGE_GER) = 99;
+    R_B_LABEL_SCALE(LANGUAGE_FRA) = 109;
+    R_B_LABEL_X(LANGUAGE_ENG) = B_BUTTON_X - 9;
+    R_B_LABEL_X(LANGUAGE_GER) = B_BUTTON_X - 11;
+    R_B_LABEL_X(LANGUAGE_FRA) = B_BUTTON_X - 12;
+    R_B_LABEL_Y(LANGUAGE_ENG) = B_BUTTON_Y + 6;
+    R_B_LABEL_Y(LANGUAGE_GER) = B_BUTTON_Y + 5;
+    R_B_LABEL_Y(LANGUAGE_FRA) = B_BUTTON_Y + 5;
+    R_A_LABEL_Z(LANGUAGE_ENG) = -380;
+    R_A_LABEL_Z(LANGUAGE_GER) = -360;
+    R_A_LABEL_Z(LANGUAGE_FRA) = -350;
+    R_KALEIDO_UNK1(LANGUAGE_ENG) = -48;
+    R_KALEIDO_UNK1(LANGUAGE_GER) = 16;
+    R_KALEIDO_UNK1(LANGUAGE_FRA) = -62;
+    R_KALEIDO_UNK2(LANGUAGE_ENG) = 22;
+    R_KALEIDO_UNK2(LANGUAGE_GER) = -84;
+    R_KALEIDO_UNK2(LANGUAGE_FRA) = 20;
+    R_KALEIDO_UNK3(LANGUAGE_ENG) = -53;
+    R_KALEIDO_UNK3(LANGUAGE_GER) = 40;
+    R_KALEIDO_UNK3(LANGUAGE_FRA) = -64;
+    R_KALEIDO_UNK4(LANGUAGE_ENG) = 47;
+    R_KALEIDO_UNK4(LANGUAGE_GER) = -84;
+    R_KALEIDO_UNK4(LANGUAGE_FRA) = 44;
+    R_KALEIDO_UNK5(LANGUAGE_ENG) = -42;
+    R_KALEIDO_UNK5(LANGUAGE_GER) = 32;
+    R_KALEIDO_UNK5(LANGUAGE_FRA) = -45;
+    R_KALEIDO_UNK6(LANGUAGE_ENG) = -37;
+    R_KALEIDO_UNK6(LANGUAGE_GER) = 30;
+    R_KALEIDO_UNK6(LANGUAGE_FRA) = -50;
 #endif
 
     R_DGN_MINIMAP_X = 204;

N64DX/oot-dx

src/code/z_construct.c
@@ -349,18 +349,18 @@ void Regs_InitDataImpl(void) {
     YREG(47) = 0;
 
 #if OOT_NTSC
-    R_KALEIDO_UNK1(0) = -45;
-    R_KALEIDO_UNK1(1) = -48;
-    R_KALEIDO_UNK2(0) = 16;
-    R_KALEIDO_UNK2(1) = 22;
-    R_KALEIDO_UNK3(0) = -55;
-    R_KALEIDO_UNK3(1) = -53;
-    R_KALEIDO_UNK4(0) = 43;
-    R_KALEIDO_UNK4(1) = 47;
-    R_KALEIDO_UNK5(0) = -33;
-    R_KALEIDO_UNK5(1) = -42;
-    R_KALEIDO_UNK6(0) = -33;
-    R_KALEIDO_UNK6(1) = -37;
+    R_KALEIDO_UNK1(LANGUAGE_JPN) = -45;
+    R_KALEIDO_UNK1(LANGUAGE_ENG) = -48;
+    R_KALEIDO_UNK2(LANGUAGE_JPN) = 16;
+    R_KALEIDO_UNK2(LANGUAGE_ENG) = 22;
+    R_KALEIDO_UNK3(LANGUAGE_JPN) = -55;
+    R_KALEIDO_UNK3(LANGUAGE_ENG) = -53;
+    R_KALEIDO_UNK4(LANGUAGE_JPN) = 43;
+    R_KALEIDO_UNK4(LANGUAGE_ENG) = 47;
+    R_KALEIDO_UNK5(LANGUAGE_JPN) = -33;
+    R_KALEIDO_UNK5(LANGUAGE_ENG) = -42;
+    R_KALEIDO_UNK6(LANGUAGE_JPN) = -33;
+    R_KALEIDO_UNK6(LANGUAGE_ENG) = -37;
 #else
     // Same as above, although these regs are now unused for PAL versions
     YREG(48) = -45;
@@ -448,24 +448,24 @@ void Regs_InitDataImpl(void) {
     ZREG(47) = 1;
 
 #if OOT_NTSC && !OOT_NTSC_N64
-    R_START_LABEL_DD(0) = 86;
-    R_START_LABEL_DD(1) = 100;
+    R_START_LABEL_DD(LANGUAGE_JPN) = 86;
+    R_START_LABEL_DD(LANGUAGE_ENG) = 100;
     R_START_LABEL_WIDTH = 0;
     R_START_LABEL_HEIGHT = 0;
-    R_START_LABEL_Y(0) = 21;
-    R_START_LABEL_Y(1) = 20;
-    R_START_LABEL_X(0) = 122;
-    R_START_LABEL_X(1) = 120;
+    R_START_LABEL_Y(LANGUAGE_JPN) = 21;
+    R_START_LABEL_Y(LANGUAGE_ENG) = 20;
+    R_START_LABEL_X(LANGUAGE_JPN) = 122;
+    R_START_LABEL_X(LANGUAGE_ENG) = 120;
 #else
-    R_START_LABEL_DD(0) = 100;
-    R_START_LABEL_DD(1) = 89;
-    R_START_LABEL_DD(2) = 92;
-    R_START_LABEL_Y(0) = 20;
-    R_START_LABEL_Y(1) = 20;
-    R_START_LABEL_Y(2) = 20;
-    R_START_LABEL_X(0) = 120;
-    R_START_LABEL_X(1) = 119;
-    R_START_LABEL_X(2) = 119;
+    R_START_LABEL_DD(LANGUAGE_ENG) = 100;
+    R_START_LABEL_DD(LANGUAGE_GER) = 89;
+    R_START_LABEL_DD(LANGUAGE_FRA) = 92;
+    R_START_LABEL_Y(LANGUAGE_ENG) = 20;
+    R_START_LABEL_Y(LANGUAGE_GER) = 20;
+    R_START_LABEL_Y(LANGUAGE_FRA) = 20;
+    R_START_LABEL_X(LANGUAGE_ENG) = 120;
+    R_START_LABEL_X(LANGUAGE_GER) = 119;
+    R_START_LABEL_X(LANGUAGE_FRA) = 119;
 #endif
 #if OOT_NTSC_N64
     R_START_LABEL_DD(3) = 86;
@@ -612,14 +612,14 @@ void Regs_InitDataImpl(void) {
     WREG(7) = 0;
 
 #if OOT_NTSC
-    R_B_LABEL_SCALE(0) = 100;
-    R_B_LABEL_SCALE(1) = 109;
-    R_B_LABEL_X(0) = B_BUTTON_X - 9;
-    R_B_LABEL_X(1) = B_BUTTON_X - 12;
-    R_B_LABEL_Y(0) = 23;
-    R_B_LABEL_Y(1) = 22;
-    R_A_LABEL_Z(0) = -380;
-    R_A_LABEL_Z(1) = -350;
+    R_B_LABEL_SCALE(LANGUAGE_JPN) = 100;
+    R_B_LABEL_SCALE(LANGUAGE_ENG) = 109;
+    R_B_LABEL_X(LANGUAGE_JPN) = B_BUTTON_X - 9;
+    R_B_LABEL_X(LANGUAGE_ENG) = B_BUTTON_X - 12;
+    R_B_LABEL_Y(LANGUAGE_JPN) = 23;
+    R_B_LABEL_Y(LANGUAGE_ENG) = 22;
+    R_A_LABEL_Z(LANGUAGE_JPN) = -380;
+    R_A_LABEL_Z(LANGUAGE_ENG) = -350;
 #else
     // Same as above, although these regs are now unused in PAL versions
     WREG(8) = 100;
@@ -654,36 +654,36 @@ void Regs_InitDataImpl(void) {
     WREG(36) = 0;
 
 #if OOT_PAL || OOT_NTSC_N64
-    R_B_LABEL_SCALE(0) = 100;
-    R_B_LABEL_SCALE(1) = 99;
-    R_B_LABEL_SCALE(2) = 109;
-    R_B_LABEL_X(0) = B_BUTTON_X - 10;
-    R_B_LABEL_X(1) = B_BUTTON_X - 9;
-    R_B_LABEL_X(2) = B_BUTTON_X - 11;
-    R_B_LABEL_Y(0) = B_BUTTON_Y + 6;
-    R_B_LABEL_Y(1) = B_BUTTON_Y + 5;
-    R_B_LABEL_Y(2) = B_BUTTON_Y + 5;
-    R_A_LABEL_Z(0) = -380;
-    R_A_LABEL_Z(1) = -360;
-    R_A_LABEL_Z(2) = -350;
-    R_KALEIDO_UNK1(0) = -48;
-    R_KALEIDO_UNK1(1) = 16;
-    R_KALEIDO_UNK1(2) = -62;
-    R_KALEIDO_UNK2(0) = 22;
-    R_KALEIDO_UNK2(1) = -84;
-    R_KALEIDO_UNK2(2) = 20;
-    R_KALEIDO_UNK3(0) = -53;
-    R_KALEIDO_UNK3(1) = 40;
-    R_KALEIDO_UNK3(2) = -64;
-    R_KALEIDO_UNK4(0) = 47;
-    R_KALEIDO_UNK4(1) = -84;
-    R_KALEIDO_UNK4(2) = 44;
-    R_KALEIDO_UNK5(0) = -42;
-    R_KALEIDO_UNK5(1) = 32;
-    R_KALEIDO_UNK5(2) = -45;
-    R_KALEIDO_UNK6(0) = -37;
-    R_KALEIDO_UNK6(1) = 30;
-    R_KALEIDO_UNK6(2) = -50;
+    R_B_LABEL_SCALE(LANGUAGE_ENG) = 100;
+    R_B_LABEL_SCALE(LANGUAGE_GER) = 99;
+    R_B_LABEL_SCALE(LANGUAGE_FRA) = 109;
+    R_B_LABEL_X(LANGUAGE_ENG) = B_BUTTON_X - 10;
+    R_B_LABEL_X(LANGUAGE_GER) = B_BUTTON_X - 9;
+    R_B_LABEL_X(LANGUAGE_FRA) = B_BUTTON_X - 11;
+    R_B_LABEL_Y(LANGUAGE_ENG) = B_BUTTON_Y + 6;
+    R_B_LABEL_Y(LANGUAGE_GER) = B_BUTTON_Y + 5;
+    R_B_LABEL_Y(LANGUAGE_FRA) = B_BUTTON_Y + 5;
+    R_A_LABEL_Z(LANGUAGE_ENG) = -380;
+    R_A_LABEL_Z(LANGUAGE_GER) = -360;
+    R_A_LABEL_Z(LANGUAGE_FRA) = -350;
+    R_KALEIDO_UNK1(LANGUAGE_ENG) = -48;
+    R_KALEIDO_UNK1(LANGUAGE_GER) = 16;
+    R_KALEIDO_UNK1(LANGUAGE_FRA) = -62;
+    R_KALEIDO_UNK2(LANGUAGE_ENG) = 22;
+    R_KALEIDO_UNK2(LANGUAGE_GER) = -84;
+    R_KALEIDO_UNK2(LANGUAGE_FRA) = 20;
+    R_KALEIDO_UNK3(LANGUAGE_ENG) = -53;
+    R_KALEIDO_UNK3(LANGUAGE_GER) = 40;
+    R_KALEIDO_UNK3(LANGUAGE_FRA) = -64;
+    R_KALEIDO_UNK4(LANGUAGE_ENG) = 47;
+    R_KALEIDO_UNK4(LANGUAGE_GER) = -84;
+    R_KALEIDO_UNK4(LANGUAGE_FRA) = 44;
+    R_KALEIDO_UNK5(LANGUAGE_ENG) = -42;
+    R_KALEIDO_UNK5(LANGUAGE_GER) = 32;
+    R_KALEIDO_UNK5(LANGUAGE_FRA) = -45;
+    R_KALEIDO_UNK6(LANGUAGE_ENG) = -37;
+    R_KALEIDO_UNK6(LANGUAGE_GER) = 30;
+    R_KALEIDO_UNK6(LANGUAGE_FRA) = -50;
 #endif
 #if OOT_NTSC_N64
     R_B_LABEL_SCALE(3) = 100;