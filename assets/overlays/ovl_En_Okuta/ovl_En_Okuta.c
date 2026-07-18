d48a773a0ec4341c66933fb37ca90024450574fb
Admentus <robin.hallink@gmail.com>
2026-07-19 00:57:04 +0200

    Ikana Canyon 3

--- assets/overlays/ovl_En_Okuta/ovl_En_Okuta.c
+++
@@ -1,8 +0,0 @@
-#include "ovl_En_Okuta.h"
-
-#include "gfx.h"
-
-Gfx gOctorokBlueMaterialDL[] = {
-    gsDPSetPrimColor(0, 0, 0, 200, 155, 255),
-    gsSPEndDisplayList(),
-};
--- assets/overlays/ovl_En_Okuta/ovl_En_Okuta.h
+++
@@ -1,6 +0,0 @@
-#ifndef OVL_EN_OKUTA_H
-#define OVL_EN_OKUTA_H
-
-extern Gfx gOctorokBlueMaterialDL[];
-
-#endif
