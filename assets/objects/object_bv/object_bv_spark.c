e8f58b776d2a944076648bcd93f5d2ba0640c0c9
GhostlyDark <31742919+GhostlyDark@users.noreply.github.com>
2026-09-05 15:33:41 +0200

    Fix Barinade spark being cut off

---
+++ assets/objects/object_bv/object_bv_spark.c
@@ -0,0 +1,9 @@
+#include "object_bv_spark.h"
+
+Gfx gBarinadeDL_0156A0_IA8[] = {
+#include "assets/objects/object_bv/gBarinadeDL_0156A0_IA8.inc.c"
+};
+
+u64 object_bv_012260_Tex_IA8[] = {
+#include "assets/objects/object_bv/object_bv_012260_Tex.ia8.inc.c"
+};
