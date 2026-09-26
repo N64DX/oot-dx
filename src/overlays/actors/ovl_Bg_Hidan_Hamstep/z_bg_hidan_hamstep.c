03d4a88e35da52862d78ea1be7919b2b27370524
GhostlyDark <31742919+GhostlyDark@users.noreply.github.com>
2026-09-26 14:02:29 +0200

    Add chime SFX from later revisions
    Play chimes for the Fire Temple stairs and the Forest Temple twisted corridor on all ROM revisions.

--- src/overlays/actors/ovl_Bg_Hidan_Hamstep/z_bg_hidan_hamstep.c
+++ src/overlays/actors/ovl_Bg_Hidan_Hamstep/z_bg_hidan_hamstep.c
@@ -394,11 +394,9 @@ void func_80888A58(BgHidanHamstep* this, PlayState* play) {
                 Rumble_Request(SQ(100.0f), 255, 20, 150);
                 func_808884C8(this, play);
 
-#if OOT_VERSION >= PAL_1_0
                 if (PARAMS_GET_U(this->dyna.actor.params, 0, 8) == 5) {
                     Sfx_PlaySfxCentered(NA_SE_SY_CORRECT_CHIME);
                 }
-#endif
 
                 PRINTF("B(%d)\n", this->dyna.actor.params);
             }
